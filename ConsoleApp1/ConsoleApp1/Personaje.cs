0cc17e65128f98b7588b7397e544ad6216a53232
Samperc <santiperezmoge@gmail.com>
2022-06-13 13:17:25 -0300

    juego

---
+++ ConsoleApp1/ConsoleApp1/Personaje.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    public enum Tipos
+    {
+        Ballestero,
+        Arquero,
+        Espadachin,
+        Piquero,
+        Campesino,
+        Mandoble
+    }
+    public class Personaje
+    {
+        public class caracteristicas
+        {
+            int velocidad;
+            int Destreza;
+            int Fuerza;
+            int Nivel;
+            int Armadura;
+            public caracteristicas(int velocidad, int destreza, int fuerza, int nivel, int armadura)
+            {
+                this.velocidad = velocidad;
+                Destreza = destreza;
+                Fuerza = fuerza;
+                Nivel = nivel;
+                Armadura = armadura;
+            }
+        }
+        
+        public class datos
+        {
+            string Tipo;
+            string Nombre;
+            string Apodo;
+            int Edad;
+            int Salud;
+
+            public datos(string tipo, string nombre, string apodo, int edad, int salud)
+            {
+                Tipo = tipo;
+                Nombre = nombre;
+                Apodo = apodo;
+                Edad = edad;
+                Salud = salud;
+            }
+        } 
+
+        
+        
+    }
+}
---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Microsoft.VisualBasic.Interaction.InputBox("Ingresa un numero", "Input", "0000");
+            List<Personaje> P=new List<Personaje>;
+            Personaje P1=new Personaje();
+            P1.caract
+        }
+
+
+        void empezarJuego(List<Personaje> P)
+        {
+          for (int i = 0; i < P.Count; i++)
+			{
+                Random Aleatorio=new Random();
+              
+                
+			}    
+        }
+    }
+}
